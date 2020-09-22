c2378c3b0bec747babbfa221b0002ddd11e188bb
Adigoon <Alex.opakunle@gmail.com>
2020-09-22 21:45:04 +0100

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -5,7 +5,7 @@ public class reverse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		char c1, c2, c3, c4;
+		char c1, c2, c3, c4, c5;
 
 			Scanner s = new Scanner(System.in);
 		
@@ -13,7 +13,8 @@ public class reverse {
 		c2 = s.findWithinHorizon(".", 0).charAt(0);
 		c3 = s.findWithinHorizon(".", 0).charAt(0);
 		c4 = s.findWithinHorizon(".", 0).charAt(0);
-			
+		c5 = s.findWithinHorizon(".", 0).charAt(0);	
+			Stsrem.out,print(c5);
 			System.out.print(c4);
 			System.out.print(c3);
 			System.out.print(c2);

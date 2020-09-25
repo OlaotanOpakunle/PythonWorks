4f7d3472c62f99e1818961875c0e6311ad67571e
Adigoon <Alex.opakunle@gmail.com>
2020-09-25 23:00:16 +0100

    Update starsToSee.java

--- starsToSee.java
+++ starsToSee.java
@@ -5,20 +5,16 @@ public class starsToSee {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		char response;
 		int num;
 		int show;
 		
 	
 			
 			
-		System.out.print("Would you like to see a row of stars?: ");
 	
-		response = s.findWithinHorizon(".", 0).charAt(0);
 		System.out.print("How many stars do you want to see?: ");
 		num = s.nextInt();
 		
-		while(response == 'Y' || response == 'y' && response != 'N' || response != 'n' ) {
 		
 		
 		show = 0;
@@ -47,3 +43,4 @@ public class starsToSee {
 	
  
 
+// fixed it!
